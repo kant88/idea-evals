251b810d245a2d1dbafaa3b7da9db3785ed5a386
kant88 <kom88aki@yahoo.co.jp>
2017-08-16 14:06:10 +0000

    googleana

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,13 @@
 
         <title>Idea-Evals</title>
         <!-- Google Tag Manager -->
-        
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N545JT9');</script>
         <!-- End Google Tag Manager -->
-       
+      
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -18,7 +22,8 @@
     </head>
     <body>
         <!-- Google Tag Manager (noscript) -->
-        
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N545JT9"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <!-- End Google Tag Manager (noscript) -->
        
         @include('commons.navbar')
