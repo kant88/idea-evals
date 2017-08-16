b01723cf7b5c16dbe4f966f93e5fbc10f13cfbfb
kant88 <kom88aki@yahoo.co.jp>
2017-08-16 12:51:03 +0000

    launch

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Idea-CS</title>
+        <title>Idea-Evals</title>
         <!-- Google Tag Manager -->
         
         <!-- End Google Tag Manager -->
